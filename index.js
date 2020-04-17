1fd0ddc36bd5c3b480b14b3a6788a308918ceee3
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-04-16 21:22:53 -0400

    Changes the port to PORT

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ const postRouter = require('./posts/postRouter.js');
 require('dotenv').config();
 
 const server = express();
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 server.use(express.json());
 server.use('/api/users/',userRouter);
 server.use('/api/posts/',postRouter);
 server.use(logger({format : "long"}))
+
+server.get('/',(req,res) => {
+   res.status(200).json({
+       message: `Welcome to ${process.env.COHORT}`
+   })
+})
 
 server.use((req, res) => {
 	res.status(404).json({
@@ -28,7 +34,7 @@ server.use((err, req, res, next) => {
 
 server.listen(port,() => {
     console.log(`Server running at http://localhost:${port}`)
-    console.log(`Output for Cross ENV COHORT:${process.env.COHORT}`)
+    //console.log(`Output for Cross ENV COHORT:${process.env.COHORT}`)
 })
 
 
